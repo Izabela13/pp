593d3d8c30adcf7dea34b1d0f6cc45ab85047587
Izabela13 <61088502+Izabela13@users.noreply.github.com>
2023-02-23 17:53:45 +0100

    Add files via upload

---
+++ 6.2. Lab6/lab6_1-3_exercise_once_again.py
@@ -0,0 +1,34 @@
+# # print("Przed Tobą możliwość narysowania prostokąta zbudowanego ze znaku i wymiarów podanych przez Ciebie.")
+# #
+# # sign = input("Podaj znak: ")
+# # columns = input("Podaj liczbę kolumn: ")
+# # rows = input("Podaj liczbę wierszy: ")
+# #
+# # print((sign * int(columns) + "\n") * int(rows))
+# #
+# #
+#
+#
+# print("Zostaniesz poproszony o podanie dwóch liczb całkowitych.")
+# print(
+# "Na podanych przez Ciebie liczbach zostaną wykonane następujące działania: \n"
+# "- dodawanie, \n"
+# "- odejmowanie, \n"
+# "- mnożenie, \n"
+# "- dzielenie, \n"
+# "- operacja modulo. \n")
+#
+# a = int(input("Podaj pierwszą liczbę całkowitą: "))
+# b = int(input("Podaj drugą liczbę całkowitą: "))
+#
+# print("A oto operacje na podanych przez Ciebie liczbach: ")
+# print("- dodawanie: "   + str(a) + " + " + str(b) + " = " + str(a + b) + ".")
+# print("- odejmowanie: " + str(a) + " - " + str(b) + " = " + str(a - b) + ".")
+# print("- mnożenie: "    + str(a) + " * " + str(b) + " = " + str(a * b) + ".")
+# print("- dzielenie: "   + str(a) + " / " + str(b) + " = " + str(a / b) + ".")
+# print("- modulo: "      + str(a) + " % " + str(b) + " = " + str(a % b) + ".")
+
+
+print("+" + "-+" * 9 + "\n" +
+      "| " * 10 + "\n" +
+      "+" + "-+" * 9 )
