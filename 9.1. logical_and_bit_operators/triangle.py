f8b9637c9d41b1ea9c0c4348f9b95eac5ecbdfba
Izabela13 <61088502+Izabela13@users.noreply.github.com>
2023-02-23 18:18:13 +0100

    Update triangle.py

--- 9.1. logical_and_bit_operators/triangle.py
+++ 9.1. logical_and_bit_operators/triangle.py
@@ -1,4 +1,8 @@
-# Pytamy użytkownika o 3 odcinki. Na tej podstawie będziemy oceniać, czy da się zbudować z nich trójkąt
+# Pytamy użytkownika o 3 odcinki. Na tej podstawie będziemy oceniać, czy da się zbudować z nich trójkąt.
+# Jeżeli z trzech podanych długości odcinków można zbudować trójkąt, to ocenimy, jaki to będzie trójkąt.
+# Będziemy oceniać trójkąt pod względem następujących charakterystyk:
+# 1) czy ze względu na boki będzie to trójkąt równoboczny, różnoboczny czy równoramienny
+# 2) czy ze względu na kąty będzie to trójkąt prostokątny, ostrokątny czy rozwartokątny
 
 print("Podaj długości 3 odcinków (liczby całkowite): ")
 a = int(input("a: "))
@@ -7,9 +11,9 @@ c = int(input("c: "))
 
 
 # Weryfikacja, czy z tych trzech odcinków można zbudować trójkąt.
-
-if (a + b > c and b + c > a and c + a > b):
-    print("Z tych odcinków można zbudować trójkąt", end = " ")
+# Aby z trzech odcinków dało się zbudować trójkąt, suma każdego z dwóch odcinków musi być większa od długości trzeciego.
+if (a + b > c and b + c > a and c + a > b): # musi być spełniona każda wariacja sumy dwóch odcinków względem trzeciego.
+    print("Z tych odcinków można zbudować trójkąt", end = " ") # nie przechodzimy do nowej linii, tutaj podamy, jaki trójkąt można zbudować
 
     if a == b and a == c and c == b:
         print("równoboczny", end = " ")
