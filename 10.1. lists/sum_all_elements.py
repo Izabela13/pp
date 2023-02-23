7582cca62a36981935135b52a69bd883f93f17c8
Izabela13 <61088502+Izabela13@users.noreply.github.com>
2023-02-23 18:28:26 +0100

    Update sum_all_elements.py

--- 10.1. lists/sum_all_elements.py
+++ 10.1. lists/sum_all_elements.py
@@ -1,7 +1,12 @@
-sum = 0
+# Polecenie: zsumuj wszystkie elementy.
 
-numbers = [1, 2, 5, 77, 85, 2, 2, 2, 2, 3234, 2323, 23]
+sum = 0 # na początku suma równa jest 0
 
-for e in numbers:
-    sum += e
+numbers = [1, 2, 5, 77, 85, 2, 2, 2, 2, 3234, 2323, 23] # lista liczb do zsumowania
+
+for e in numbers: # sumowanie elementów w pętli for - nie potrzebujęt tutaj indeksów
+    sum += e # polecenie - dodawaj element do sumy
 print("Suma wszystkich elementów listy", numbers, "to", str(sum) + ".")
+
+# Można też było po prostu zsumować:
+# print(sum(numbers)) # tylko uwaga na konflikt nazw
