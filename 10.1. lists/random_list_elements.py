b51eb7b36bb5d95bf9242f3eb03a80918f77e877
Izabela13 <61088502+Izabela13@users.noreply.github.com>
2023-02-23 18:27:55 +0100

    Update random_list_elements.py

--- 10.1. lists/random_list_elements.py
+++ 10.1. lists/random_list_elements.py
@@ -1,9 +1,10 @@
-import random
+# Zadanie: wygenerować 10 pseudolosowych liczb, umieścić w liście i wyświetlić.
+import random # zaimportowanie modułu "random"
 
-numbers = []
+numbers = [] # wylosowane numery będą zapisywane w liście
 
-for i in range(10):
-    number = random.randint(1, 100)
-    numbers.append(number)
+for i in range(10): # w pętli for będzie losowane w sumie 10 liczb
+    number = random.randint(1, 100) # losowanie liczb od 1 do 100
+    numbers.append(number) # każda liczba będzie zapisywana do zmiennej number (metoda append)
 
 print(numbers)
