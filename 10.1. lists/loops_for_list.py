945f1c6ded0c9c5d63f461399a06be34916061c6
Izabela13 <61088502+Izabela13@users.noreply.github.com>
2023-02-23 18:27:31 +0100

    Update loops_for_list.py

--- 10.1. lists/loops_for_list.py
+++ 10.1. lists/loops_for_list.py
@@ -1,18 +1,20 @@
-#pętla for
+# iterowanie po listach
+
+# pętla for - iterowanie po indeksach
 
 fruits = ["banan", "jabłko", "czereśnia"]
 
-for i in range(len(fruits)):
+for i in range(len(fruits)): # funkcja len() zwróci ilość elementów - tyle razy przejdę przez pętlę, ile mam elementów w liście.
     print(fruits[i]) # odwołuję się do każdego owocu za pomocą jego indeksu
 
 print()
 
 for i in range(len(fruits)):
-    print(i, fruits[i]) # odwołuję się do każdego owocu za pomocą jego indeksu
+    print(i, fruits[i]) # odwołuję się do każdego owocu za pomocą jego indeksu, mogę też wyświetlić ten indeks.
 
 print()
 
 # Pętla for bez indeksów:
 
-for f in fruits:
-    print(f)
+for f in fruits: # iteracja po liście "tak po prostu"
+    print(f) # nie mam tutaj dostępu do indeksów
