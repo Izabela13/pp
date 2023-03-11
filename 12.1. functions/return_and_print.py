249d9bb2cbf8db91bf70b5bcb564251808523039
Izabela13 <61088502+Izabela13@users.noreply.github.com>
2023-03-11 12:28:43 +0100

    Add files via upload

--- 12.1. functions/return_and_print.py
+++ 12.1. functions/return_and_print.py
@@ -1,21 +1,40 @@
+# # Czym się różni wprowadzenie do funkcji print() a return.
 def my_name():
     return "Marcin" # Ta funkcja zwraca jakąś wartość i nie drukuje nic
 
 def show_my_name():
-    print("Marcin") # Ta funkcja też zwraca jakąś wartość i ją drukuje
-    return None
+    print("Marcin") # Ta funkcja też zwraca jakąś wartość i wcześniej jeszcze coś drukuje
+    return None # Każda funkcja zwraca jakąś wartość. "return None" jest opcjonalne.
+
+# Jaka jest różnica między funkcją "my_name" a "show_my_name"?
+# Funkcja "show_my_name" jeszcze coś drukuje.
 
 
 # Wywołanie funkcji:
-my_name() # nie ma znaczenia, czy funkcja coś zwróci
-# po wywołaniu nic się nie wydarzyło, bo nie ma komendy print()
+my_name() # nic się nie pojawiło.
+# Nie ma znaczenia, czy funkcja coś zwróci. Jeżeli ta funkcja coś zwróci, to ignorujemy to.
+# Po wywołaniu nic się nie wydarzyło, bo nie ma komendy print()
 
-result = my_name() # jednak chcę wydrukować
-print(result)
+# Nie trzeba ingnorować tego, co zwraca ta funkcja.
+result = my_name() # jednak chcę wydrukować, to co zawiera funkcja "my_name", więc podstawiam ją do jakiejś zmiennej.
+print(result) # drukowanie jawne
+# Zadaniem funkcji "my_name" jest tylko zwrócić jakiś ciąg znaków (ale go nie wyświetlać).
+# Po wywołaniu tej funkcji na konsoli nie pojawi się nic, ponieważ nikt nie podał komendy "wydrukuj".
+# Jeżeli podstawimy funkcję "my_name" do zmiennej "result", to samo podstawienie nie spowoduje wydrukowania wartości.
+# Dopiero wydanie polecenia print(result) spowoduje wydrukowanie jakiejś wartości na konsoli.
 
 
 # Podejście nr 2
-# show_my_name ma wbite "print"
+# Funkcja "show_my_name" podczas wykonania ma wbite "print". Mimo tego, że wynik zostanie zapisany do zmiennej "result",
+# to oprócz tego, że jakiś rezultat zapisze się w zmiennej "result" to w międzyczasie, podczas wywoływania funkcji
+# "show_my_name" wywoła się akcja "print" i pojawi się wartość, która była przechowywana w funkcji print().
 result = show_my_name()
-print(result)
-# Zawsze będzie się wyświetlać "Marcin", bo wbita jest funkcja "print". Ale ta funkcja zwróci "None".
+print(result) # Wynik w pierwszej linijce: Marcin
+              # Wynik w drugiej linijce: None
+# Skąd się wzięło "Marcin" i skąd się wzięło "None"?
+# "Marcin" wzięło się stąd, że podczas tego podstawiania wydrukowało się "Marcin".
+# "None" wzięło się stąd, że funkcja "show_my_name" zwraca (return) wartość "None" i jeszcze drukuje tę wartość.
+# Pod return można podstawić coś innego, np. 1234565433. Wówczas w miejsce "None" wydrukuje się wartość "1234565433".
+
+# Podczas działania programu najczęściej nie wyświetla się funkcji. Zwykle dopiero, gdy wykorzystuje się funkcję,
+# wówczas przekazuje się ją do jakiegoś innego miejsca, np. do wyświetlenia na stronie, przekazania mailowe, etc.
