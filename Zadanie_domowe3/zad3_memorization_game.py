2d00f7a6469c4c4db47a2f80b144641a462b8d88
Izabela13 <61088502+Izabela13@users.noreply.github.com>
2023-03-25 07:09:13 +0100

    Add files via upload

--- Zadanie_domowe3/zad3_memorization_game.py
+++ Zadanie_domowe3/zad3_memorization_game.py
@@ -7,7 +7,7 @@ Rozgrywka kończy się gdy, któryś z graczy popełni błąd.
 
 Na ekranie komputera przy każdej turze należy wymazać ekran np. przez wyświetlenie 100 pustych wierszy.
 """
-
+# TODO
 
 # Wprowadzanie graczy do systemu
 players_counter = 1
@@ -46,8 +46,6 @@ def put_word():
         players_words.append(word)
     return players_words
 
-# print(put_word())
-
 
 # # Funkcja przechwytująca powtarzanie słów
 def enumerate_words():
@@ -61,31 +59,42 @@ def enumerate_words():
         if which_words == "":
             break
 
-    return repetition_words
+    return repetition_words[:-1]
 
 
-# print(enumerate_words())
-
-
-# # Rozgrywka
-
+# # Funkcja rozpoczynająca rozgrywkę
 def start():
 
     print(players_list[-1] + ", rozpoczynasz.")
     return put_word()
 
 
+# # Funkcja porównująca słowa wprowadzone do systemu z powtarzanymi
+def comparison(players_words):
+    if players_words != enumerate_words():
+        return print("Koniec gry.")
+    else:
+        return True
+
+
+# # Rozgrywka
 counter_2 = 1
 
 while True:
     if counter_2 == 1:
         start()
         counter_2 += 1
+        print("\n" * 100)
     else:
         for i in range(len(players_list)):
-            print(players_list[i], enumerate_words())
-            if players_words == enumerate_words():
-                print(put_word())
+            print(players_list[i])
+            if comparison(players_words) == True:
+                put_word()
+                counter_2 += 1
+                print("\n" * 100)
             else:
-                print("Koniec gry.")
-        counter_2 += 1
+                print(players_list[i] + ",odpadasz z gry.")
+                print("\n" * 100)
+                counter_2 += 1
+                if len(players_list) == 2:
+                    break
