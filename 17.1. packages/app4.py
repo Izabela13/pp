27f1f3d01121458a6d6ad9b8c3593a4b83f19694
Izabela13 <61088502+Izabela13@users.noreply.github.com>
2023-04-12 16:55:22 +0200

    Add files via upload

---
+++ 17.1. packages/app4.py
@@ -0,0 +1,9 @@
+import sys
+
+# Lokalizacja pliku zip na komputerze uczelnianym:
+# sys.path.append("\\\\fs1\\FolderRedir-Student\\jajkiewi\\Desktop\\package2.zip")
+
+sys.path.append("C:\\Kursy\\Python\\packages\\package2_2.zip")
+
+from package2 import module1
+module1.introduce()
