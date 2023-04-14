a7eeddcf0d4535f9af02e1c99595675c365365a0
Izabela13 <61088502+Izabela13@users.noreply.github.com>
2023-04-15 00:10:07 +0200

    Add files via upload

--- 22.2. Lab22/lab22_3_Book_class.py
+++ 22.2. Lab22/lab22_3_Book_class.py
@@ -8,23 +8,43 @@
 
 class Book:
 
-    def __init__(self, title, author, issue):
+    def __init__(self, title, author, issue, publisher):
         self.__title = title
         self.__author = author
         self.__issue = issue
+        self.__publisher = publisher
 
-    def get_book_name(self):
-        return self.__title
+    def show_short_book_info(self):
+        print(self.__title, "\t", self.__author)
 
-    def getfullname(self):
-        return self.__author
+    def show_all_book_info(self):
+        print(self.__title, "\t", self.__author, "\t", self.__issue, "\t" * 4, self.__publisher)
 
-    def getage(self):
-        return self.__issue
+#######################################################################################################################
 
-
-book = []
-book.append(Book("Bastion", "Stephen King", 1986))
-book.append(Book("W górach szaleństwa", "Lovecraft", 1931))
+def gap(n):
+    return "\t" * n
 
 
+books = []
+books.append(Book("Bastion \t\t\t\t",         "Stephen King \t\t\t",       1986,   "Albatros"))
+books.append(Book("W górach szaleństwa \t",   "Howard Phillips Lovecraft", 1931,   "C&T Crime & Thriller"))
+books.append(Book("Ania z Zielonego Wzgórza", "Lucy Maud Montgomery \t",   2009,   "Wydawnictwo Literackie"))
+
+print()
+
+print("Książki. Krótka informacja.")
+print("-" * 60)
+print("TYTUŁ", gap(6), "AUTOR")
+print("-" * 60)
+for b in books:
+    b.show_short_book_info()
+
+print("\n")
+
+print("Książki. Szczegółowe informacje.")
+print("-" * 100)
+print("TYTUŁ", gap(6), "AUTOR", gap(6), "ROK WYDANIA", gap(2), "WYDAWNICTWO")
+print("-" * 100)
+for b in books:
+    b.show_all_book_info()
