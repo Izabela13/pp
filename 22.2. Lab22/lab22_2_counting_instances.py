32e2d546f9c6cb323f255c6d3983911c5b8ac25f
Izabela13 <61088502+Izabela13@users.noreply.github.com>
2023-04-14 23:23:05 +0200

    Add files via upload

--- 22.2. Lab22/lab22_2_counting_instances.py
+++ 22.2. Lab22/lab22_2_counting_instances.py
@@ -2,54 +2,21 @@
 2. Napisz klasę zliczającą wszystkie powstałe swoje instancje.
 """
 
-class Employee():
+class MyClass:
 
-    def __init__(self, firstname, lastname, age, salary):
-        self.__firstname = firstname
-        self.__lastname = lastname
-        self.__age = age
-        self.__salary = salary
-
-    def getsalary(self):
-        return self.__salary
-
-    def getfullname(self):
-        return self.__firstname + " " + self.__lastname
-
-    def getage(self):
-        return self.__age
-
-    def risesalary(self, pct=1.10):
-        return self.__salary * pct
-
-
-def CounterClass():
     counter = 0
-    if Employee:
-        counter += 1
-    return counter
 
-
-employee = []
-employee.append(Employee("Jan", "Kowalski", 25, 3800))
-employee.append(Employee("Edmund", "Kaczmarczyk", 45, 7000))
-employee.append(Employee("Natalia", "Nowak", 60, 15200))
-
-print("Lista płac")
-print("-" * 50)
-
-for e in employee:
-    print(e.getfullname(), "wiek:", e.getage(), "lat,", "pensja:", e.getsalary(), "zł")
-
-
-print("\n")
-print("Lista płac po podwyżce o 10%")
-print("-" * 50)
-
-for e in employee:
-    print(e.getfullname(), "wiek:", e.getage(), "lat,", "pensja po podwyżce:", round(e.risesalary()), "zł")
+    def __init__(self):
+        MyClass.counter += 1
 
 
 print()
-counting = CounterClass()
-print(counting)
+print("Ilość początkowa instancji:", MyClass.counter, "\n")
+
+print("Generowanie instancji:")
+for o in range(10):
+    o = MyClass()
+    print("instancja", MyClass.counter)
+
+print()
+print("Suma powstałych instancji:", MyClass.counter)
